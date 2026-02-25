3abc66f7f19770b699c9c9f99bbe6fcc5e277145
kyouseikuma <nishikou1031@gmail.com>
2026-02-25 22:08:27 +0900

    マイグレーション

--- db/migrate/20260224135336_devise_create_users.rb
+++ db/migrate/20260224135336_devise_create_users.rb
@@ -4,8 +4,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :username,           null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :affiliation,        null: false, default: ""
+      t.string :employee_number,    null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
